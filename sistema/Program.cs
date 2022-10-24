acbd6e28c135928dd2250584214ce3aaf3f2be78
marisaCa <116469315+marisaCa@users.noreply.github.com>
2022-10-23 21:53:29 -0600

    prueba domingo

---
+++ sistema/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"{nameof(MyConsoleApp)} {DateTime.UtcNow}");
+            Console.WriteLine("Press any key to close");
+            Console.ReadKey();
+        }
+    }
+}
